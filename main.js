d3118e7f07073adb1fb3bd75cc447dab7f677fd5
xypnox <xypnox@gmail.com>
2018-05-29 15:11:28 +0530

    improved the general theme added second sample post

--- main.js
+++ main.js
@@ -6,11 +6,11 @@ function getCookie(keks) {
 
 function setTheme() {
     if (getCookie('themeStyle') == 'dark') {
-        document.body.style.backgroundColor = 'rgb(28, 27, 22)';
-        document.body.style.color = 'rgb(223, 220, 205)'
+        document.body.style.backgroundColor = 'rgb(33, 30, 29)';
+        document.body.style.color = 'rgb(203, 183, 183)';
     } else {
-        document.body.style.backgroundColor = 'rgb(223, 212, 205)';
-        document.body.style.color = 'rgb(41, 34, 30)'
+        document.body.style.backgroundColor = '#fbf0d9';
+        document.body.style.color = '#5b4636'
     }
 }
 setInterval(setTheme, 100);
